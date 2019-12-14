eba434b3c95cf8899913f17a5016759f9e6518fa
AbhiDevWorks <devworksabhinab@gmail.com>
2019-12-14 08:46:16 +0530

    Debugging props code snippet in Details cmp & Reach Router Link in App header"

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Details = () => {
+const Details = props => {
   return <h1>hi!</h1>;
+  {
+    /* AWESOME FOR DEBUGGING PROPS
+      <pre>
+      <code>{JSON.stringify(props, null, 4)}</code>
+    </pre> */
+  }
 };
 
 export default Details;
